1cc037d959a631ed8fde0c32024e16911c89ab63
JEE <jeerawut.jd40@gmail.com>
2023-12-30 14:45:57 +0700

    Add Quiz To App

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import QUESTION from '../questions.js';
+
+export default function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer];
+        });
+    }
+
+    return <div id='question'>
+        <h2>{QUESTION[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+            {QUESTION[activeQuestionIndex].answers.map((answer) => (
+                <li key={answer} className="answer">
+                    <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                </li>
+            ))}
+        </ul>
+    </div>
+}
